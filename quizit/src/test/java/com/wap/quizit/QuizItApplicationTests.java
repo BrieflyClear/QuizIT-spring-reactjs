80af088bc2894bc22f35d994c4ab7a8a6fcad0e4
BrieflyClear <andrzej.przybysz01@gmail.com>
2020-03-28 12:59:32 +0100

    spring init

---
+++ quizit/src/test/java/com/wap/quizit/QuizItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wap.quizit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
